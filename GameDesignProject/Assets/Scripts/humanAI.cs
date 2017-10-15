cc73537150945dad18da4af512faa5255192b2ec
Yuliyan <y.georgiev@student.tue.nl>
2017-10-15 13:48:10 +0200

    Added chasing NPC

---
+++ GameDesignProject/Assets/Scripts/humanAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanAI : MonoBehaviour {
+
+    public float speed;
+    public Transform target;
+    public float chaseRange;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distanceToTarger = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarger < chaseRange)
+        {
+            anim.SetBool("sawPlayer", true);
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+	}
+}
